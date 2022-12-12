8b1c80ef50b222af68b53e1f45d1f08ef9651097
hfsamrts <harunfazlic@yahoo.com>
2022-12-12 07:05:58 +0100

    Initial Commit

---
+++ Delegate Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Delegate Example
+//
+//  Created by Harun Fazlic on 12. 12. 2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
